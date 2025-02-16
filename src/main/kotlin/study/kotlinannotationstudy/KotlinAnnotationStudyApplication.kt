45513d8693110e47633c3cf22ad7b7072707682d
오근혁 <oyfkr@naver.com>
2025-02-16 20:56:57 +0900

    init annotation-study project

---
+++ src/main/kotlin/study/kotlinannotationstudy/KotlinAnnotationStudyApplication.kt
@@ -0,0 +1,11 @@
+package study.kotlinannotationstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAnnotationStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinAnnotationStudyApplication>(*args)
+}
---
+++ src/test/kotlin/study/kotlinannotationstudy/KotlinAnnotationStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.kotlinannotationstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAnnotationStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
